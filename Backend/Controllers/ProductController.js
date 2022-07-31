9fda6298fec4b69b41e3a44c1a3ae68dc8e3e2ac
Anurag19-Pandey <89677430+Anurag19-Pandey@users.noreply.github.com>
2022-07-31 16:44:54 +0530

    Tenth Commit By Anurag Pandey

--- Backend/Controllers/ProductController.js
+++ Backend/Controllers/ProductController.js
@@ -1,6 +1,8 @@
 const Product = require("../Schemas/ProductSchema")
 const {ProductError} = require("../ErrorHandling/ProductError")
 const Seller = require('../Schemas/SellerSchema')
+const mongoose = require('mongoose')
+var ObjectId = require('mongoose').ObjectID;
 
 module.exports.getAllProducts = async(req,res)=>{
 
@@ -19,4 +21,14 @@ module.exports.editProduct = async(req,res)=>{
     
 
     const product = await Product.findByIdAndUpdate()
+}
+
+module.exports.singleProduct = async(req,res)=>{
+
+    const {id} = req.params
+
+    const product = await Product.findOne({_id:id})
+
+    res.send(product)
+
 }
